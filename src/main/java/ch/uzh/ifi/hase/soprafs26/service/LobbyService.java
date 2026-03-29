890c0e3b16abf014092b5c1dd3d74d9660e621cd
Thissan Iyadurai <thissan.iyadurai114@gmail.com>
2026-03-29 15:36:36 +0200

    Implemented unique lobby code generation, close #63

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
@@ -1,5 +1,9 @@
 package ch.uzh.ifi.hase.soprafs26.service;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +15,8 @@ import ch.uzh.ifi.hase.soprafs26.repository.LobbyRepository;
 @Transactional
 public class LobbyService {
 
+    private final Logger log = LoggerFactory.getLogger(LobbyService.class);
+
     private final LobbyRepository lobbyRepository;
 
     public LobbyService(@Qualifier("lobbyRepository") LobbyRepository lobbyRepository) {
@@ -18,7 +24,21 @@ public class LobbyService {
     }
 
     public Lobby createLobby(Lobby newLobby) {
-        // Logic comes in Commit 2 + 3
+        // Generate unique lobby code
+        newLobby.setLobbyCode(generateLobbyCode());
+        
+        log.debug("Created lobby with code: {}", newLobby.getLobbyCode());
         return newLobby;
+    }
+
+    private String generateLobbyCode() {
+        String code;
+        do {
+            // Generate code like "GX-7742"
+            String letters = UUID.randomUUID().toString().substring(0, 2).toUpperCase();
+            String numbers = String.valueOf((int)(Math.random() * 9000) + 1000);
+            code = letters + "-" + numbers;
+        } while (lobbyRepository.findByLobbyCode(code) != null);
+        return code;
     }
 }
