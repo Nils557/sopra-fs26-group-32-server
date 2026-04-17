2851d27e9d3751c127f9c698ed27c87177ffe8c5
faiaz18 <faiaz.islam2004gmail.com>
2026-04-17 22:35:12 +0200

    fixed deletion when refreshing page, #116

--- src/main/java/ch/uzh/ifi/hase/soprafs26/websocket/WebSocketEventListener.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/websocket/WebSocketEventListener.java
@@ -1,6 +1,5 @@
 package ch.uzh.ifi.hase.soprafs26.websocket;
 
-import ch.uzh.ifi.hase.soprafs26.service.LobbyService;
 import ch.uzh.ifi.hase.soprafs26.service.WebSocketSessionService;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,20 +9,13 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class WebSocketEventListener {
 
     private final WebSocketSessionService sessionService;
-    private final LobbyService lobbyService;
 
-    public WebSocketEventListener(WebSocketSessionService sessionService, LobbyService lobbyService) {
+    public WebSocketEventListener(WebSocketSessionService sessionService) {
         this.sessionService = sessionService;
-        this.lobbyService = lobbyService;
     }
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        String sessionId = event.getSessionId();
-        Long userId = sessionService.getUserId(sessionId);
-        sessionService.remove(sessionId);
-        if (userId != null) {
-            lobbyService.handlePlayerDisconnect(userId);
-        }
+        sessionService.remove(event.getSessionId());
     }
 }
