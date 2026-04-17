5fe925ff07c659a09e65c068cd8ef0aa2c3f8bf6
faiaz18 <faiaz.islam2004gmail.com>
2026-04-17 18:04:22 +0200

    code review for user story 1,  #116

--- src/main/java/ch/uzh/ifi/hase/soprafs26/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/UserController.java
@@ -1,55 +1,61 @@
-package ch.uzh.ifi.hase.soprafs26.controller;                                                                                                                                                           
-  
-  import java.util.ArrayList;
-  import java.util.List;
+package ch.uzh.ifi.hase.soprafs26.controller;
 
-  import org.springframework.http.HttpStatus;                                                                                                                                                               import org.springframework.web.bind.annotation.DeleteMapping;
-  import org.springframework.web.bind.annotation.GetMapping;                                                                                                                                              
-  import org.springframework.web.bind.annotation.PathVariable;
-  import org.springframework.web.bind.annotation.PostMapping;                                                                                                                                               import org.springframework.web.bind.annotation.RequestBody;
-  import org.springframework.web.bind.annotation.ResponseBody;                                                                                                                                            
-  import org.springframework.web.bind.annotation.ResponseStatus;                                                                                                                                            import org.springframework.web.bind.annotation.RestController;
-                                                                                                                                                                                                          
-  import ch.uzh.ifi.hase.soprafs26.entity.User;
-  import ch.uzh.ifi.hase.soprafs26.rest.dto.UserGetDTO;
-  import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPostDTO;                                                                                                                                                  
-  import ch.uzh.ifi.hase.soprafs26.rest.mapper.DTOMapper;                                                                                                                                                   import ch.uzh.ifi.hase.soprafs26.service.LobbyService;                                                                                                                                                  
-  import ch.uzh.ifi.hase.soprafs26.service.UserService;                                                                                                                                                     import jakarta.servlet.http.HttpSession;
-                                                                                                                                                                                                            @RestController
-  public class UserController {                                                                                                                                                                           
-  
-      private final UserService userService;
-      private final LobbyService lobbyService;
+import java.util.ArrayList;
+import java.util.List;
 
-      UserController(UserService userService, LobbyService lobbyService) {                                                                                                                                          this.userService = userService;
-          this.lobbyService = lobbyService;                                                                                                                                                               
-      }
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-      @GetMapping("/users")
-      @ResponseStatus(HttpStatus.OK)
-      @ResponseBody
-      public List<UserGetDTO> getAllUsers() {                                                                                                                                                                       List<User> users = userService.getUsers();
-          List<UserGetDTO> userGetDTOs = new ArrayList<>();                                                                                                                                               
-          for (User user : users) {                                                                                                                                                                                     userGetDTOs.add(DTOMapper.INSTANCE.convertEntityToUserGetDTO(user));
-          }                                                                                                                                                                                                         return userGetDTOs;
-      }                                                                                                                                                                                                   
-  
-      @PostMapping("/users")
-      @ResponseStatus(HttpStatus.CREATED)
-      @ResponseBody
-      public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO, HttpSession session) {
-          User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);                                                                                                                    
-          User createdUser = userService.createUser(userInput);                                                                                                                                                     session.setAttribute("userId", createdUser.getId());                                                                                                                                            
-          return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);                                                                                                                               
-      }
+import ch.uzh.ifi.hase.soprafs26.entity.User;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.UserGetDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs26.service.LobbyService;
+import ch.uzh.ifi.hase.soprafs26.service.UserService;
 
-      @PostMapping("/logout")                                                                                                                                                                                   public void logout(HttpSession session) {
-          session.invalidate();                                                                                                                                                                           
-      }
+@RestController
+public class UserController {
 
-      @DeleteMapping("/users/{id}")
-      @ResponseStatus(HttpStatus.NO_CONTENT)
-      public void deleteUser(@PathVariable Long id) {                                                                                                                                                               lobbyService.handlePlayerDisconnect(id);
-          userService.deleteUser(id);                                                                                                                                                                     
-      }
-  }
+    private final UserService userService;
+    private final LobbyService lobbyService;
+
+    UserController(UserService userService, LobbyService lobbyService) {
+        this.userService = userService;
+        this.lobbyService = lobbyService;
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<UserGetDTO> getAllUsers() {
+        List<User> users = userService.getUsers();
+        List<UserGetDTO> userGetDTOs = new ArrayList<>();
+        for (User user : users) {
+            userGetDTOs.add(DTOMapper.INSTANCE.convertEntityToUserGetDTO(user));
+        }
+        return userGetDTOs;
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public UserGetDTO createUser(@RequestBody UserPostDTO userPostDTO) {
+        User userInput = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+        User createdUser = userService.createUser(userInput);
+        return DTOMapper.INSTANCE.convertEntityToUserGetDTO(createdUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        lobbyService.handlePlayerDisconnect(id);
+        userService.deleteUser(id);
+    }
+}
